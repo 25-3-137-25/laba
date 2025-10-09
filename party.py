e636e0c0e59b1427c7fcfbb8d52876e3fac65568
butterfly-19 <ivanko7002@gmail.com>
2025-10-09 23:58:59 +0300

    the night from 9/10 to 10/10

--- party.py
+++ party.py
@@ -8,6 +8,7 @@ class Human:
         self.iq = iq
         self.damage = damage
 
+
     @property
     def zhiv(self):
         if self.hp > 0:
@@ -15,26 +16,52 @@ class Human:
         else:
             return "Персонаж мёртв."
 
+    @property
+    def yroven(self, someone):
+        if self.level >= someone.level:
+            return 1
+        else:
+            return 0
+
     def print_info(self):
         print('Имя: {}. Уровень: {}. Сила: {}. ХП: {}. Ловкость: {}. Интеллект: {}.'.format(self.name, self.level, self.strengh, self.hp, self.dexterity, self.iq))
+
     def take_damage(self, damage):
         self.hp -= damage
 
 
 
 class leader(Human):
-    def __init__(self, name, dexterity, level = 19, strengh = 100, hp = 150, iq = 140, damage = 50):
+    def __init__(self, name, dexterity, damage, stiffness, strengh, level = 19, hp = 150, iq = 140):
         super().__init__(name, dexterity, level, strengh, hp, iq, damage)
-    def hand(self, target, damage, strengh, iq):
-        target.take_damage((damage + (strengh*0.2) + (iq/50)))
-        print(f"Персонаж {self.name} ударил кулаком персонажа {target.name}, нанеся {(damage + (strengh*0.2))} урона")
+        self.stiffness = stiffness
+
+
+    def hand(self, target, damage, strengh, stiffness):
+        target.take_damage((damage + (strengh*0.2) + (stiffness/50)))
+        print(f"Персонаж {self.name} ударил кулаком персонажа {target.name}, нанеся {(damage + (strengh*0.2) + (stiffness/50))} урона")
+
     def butt(self, target, damage, dexterity):
         target.take_damage((damage + (dexterity*0.2)))
         print(f"Персонаж {self.name}, толкнул своей пятой точкой персонажа {target.name}, нанеся {(damage + (dexterity*0.2))} урона")
 
+
+
 class funny(Human):
-    def __init__(self, name, level, strengh, hp, dexterity, iq, damage):
+    def __init__(self, name, idiocy, damage, dexterity, level = 10, strengh = 1, hp = 88, iq = 1):
         super().__init__(name, level, strengh, hp, dexterity, iq, damage)
+        self.idiocy = idiocy
+
+
+    def papaya(self, target, damage, idiocy):
+        target.take_damage((damage + (idiocy*0.3)))
+        print(f"Персонаж {self.name}, настолько сильно накричал на персонажа {target.name}, что тот даже получил {(damage + (idiocy*0.3))} урона")
+
+    def licking(self, target, damage, idiocy, dexterity):
+        target.take_damage((damage + (idiocy*0.1) + (dexterity*0.2)))
+        print(f"Персонаж {self.name}, достал свой шершавый залежалый язык, вылизал им персонажа {target.name}, тем самым нанёс {(damage + (idiocy*0.1) + (dexterity*0.2))} урона")
+
+
 
 class playful(Human):
     pass
--- species.py
+++ species.py
@@ -1,6 +1,6 @@
 from party import *
 
-kevin = leader(name = "Кевин, лидер нашей отважной группки миньонов", dexterity = 50)
-bob = funny("Боб, глупый, милый, симпатичный миньончик", 11, 40, 100, 200, 150, damage = 3)
+kevin = leader(name = "Кевин, лидер нашей отважной группки миньонов", dexterity = 50, damage = 50, stiffness = 35, strengh = 60)
+bob = funny( name = "Боб, глупый, милый, симпатичный миньончик", idiocy = 70, dexterity= 30, damage = 15)
 stuart = playful("Стюарт, игривый интригант в мире миньонов", 15, 70, 125, 100, 110, damage = 5)
 
