62b21a27ab32818bbb23079aef6d3cd656590504
butterfly-19 <ivanko7002@gmail.com>
2025-10-11 15:34:15 +0300

    almost done

--- main.py
+++ main.py
@@ -2,49 +2,82 @@ from party import *
 from species import *
 from boss import *
 
-first_text=  ("ВЫ В МИРЕ МУЛЬТФИЛЬМА ГАДКИЙ Я. НА ЭТОТ РАЗ МИНЬОНЫ СТАЛИ РАБОТАТЬ САМОСТОЯТЕЛЬНО, ВЫБРАВ ПУТЬ БОРЬБЫ СО ЗЛОДЕЯМИ, А НЕ РАБОТЫ С НИМИ.\nС ПОМОЩЬЮ ТРЁХ БРАВЫХ БОЙЦОВ ВЫ ДОЛЖНЫ ОДОЛЕТЬ АДОЛЬФА ГИТЛЕРА (любые совпадения случайны, персонажи вымышленные).\nИГРА ПРОИСХОДИТ ПО ХОДАМ, КАЖДЫЙ ХОД ВЫ ДОЛЖНЫ ВЫБРАТЬ ПЕРСОНАЖА, КОТОРЫМ ХОТИТЕ АТАКОВАТЬ, И ОДНУ ИЗ ДВУХ АТАК ЗА ЭТОГО ПЕРСОНАЖА.\nПОСТАРАЙТЕСЬ УБИТЬ БОССА ЗА КАК МОЖНО МЕНЬШЕЕ КОЛИЧЕСТВО ШАГОВ.\nУДАЧИ!")
-print(first_text)
-print("")
-print("")
-print(1)
-kevin.print_info()
-print("")
-print(2)
-bob.print_info()
-print("")
-print(3)
-stuart.print_info()
-print("")
-print("")
-print("Учтите, что на каждый удар с вашей стороны приходится по 2 удара со стороны босса.")
-print("")
-print("")
+intro = (
+    "ВЫ В МИРЕ МУЛЬТФИЛЬМА ГАДКИЙ Я. НА ЭТОТ РАЗ МИНЬОНЫ СТАЛИ РАБОТАТЬ САМОСТОЯТЕЛЬНО, ВЫБРАВ ПУТЬ БОРЬБЫ СО ЗЛОДЕЯМИ, А НЕ РАБОТЫ С НИМИ.\n"
+    "С ПОМОЩЬЮ ТРЁХ БРАВЫХ БОЙЦОВ ВЫ ДОЛЖНЫ ОДОЛЕТЬ АДОЛЬФА ГИТЛЕРА (любые совпадения случайны, персонажи вымышленные).\n"
+    "ИГРА ПРОИСХОДИТ ПО ХОДАМ, КАЖДЫЙ ХОД ВЫ ДОЛЖНЫ ВЫБРАТЬ ПЕРСОНАЖА, КОТОРЫМ ХОТИТЕ АТАКОВАТЬ, И ОДНУ ИЗ ДВУХ АТАК ЗА ЭТОГО ПЕРСОНАЖА.\n"
+    "ПОСТАРАЙТЕСЬ УБИТЬ БОССА ЗА КАК МОЖНО МЕНЬШЕЕ КОЛИЧЕСТВО ШАГОВ.\n"
+    "УДАЧИ!"
+)
+print(intro)
+print()
+
+minions = {
+    "1": kevin,
+    "2": bob,
+    "3": stuart,
+    "кевин": kevin,
+    "боб": bob,
+    "стюарт": stuart,
+}
+
+for i, minion in enumerate([kevin, bob, stuart], start=1):
+    print(i)
+    minion.print_info()
+    print()
+
+
+
 while hitler.alive():
-    print(f"У вашего врага на данный момент {hitler.hp} хп")
-    print("")
+    print(f"У вашего врага на данный момент {hitler.hp} хп\n")
     print("Выберите миньона для хода")
-    print("")
-    a = input().lower()
-    characters = {"1": "Кевин", "2": "Боб", "3": "Стюарт", "кевин": "Кевин", "боб": "Боб", "стюарт": "Стюарт"}
-    if a in characters:
-        print(f"Ваш персонаж - {characters[a]}")
-        if a == "1" or "кевин":
-            if kevin.zhiv():
-                print(f"У Кевина сейчас {kevin.hp} хп")
-                print("Выберите атаку из двух: 1. Удар кулаком 2. Удар пятой точкой. Напишите номер атаки")
-                b = int(input())
-                if b == 1:
-                    kevin.hand(hitler, kevin.damage, kevin.strengh, kevin.stiffness)
-                elif b == 2:
-                    kevin.butt(hitler, kevin.damage, kevin.dexterity, kevin.stiffness)
-                hitler.attacking(kevin, hitler.damage)
-                if kevin.hp <= 0:
-                    print("Кевин погиб")
-                else:
-                    print("У Кевина теперь {kevin.hp} хп")
-            else:
-                print("Кевин мертв, выберите другого персонажа")
-    else:
+
+    choice = input().lower().strip()
+    if choice not in minions:
         print("ПЕРСОНАЖА ПОД ТАКИМ НОМЕРОМ НЕТ.")
+        continue
+
+    minion = minions[choice]
+
+    print(f"Ваш персонаж - {minion.name}")
+
+    if not minion.zhiv():
+        print(f"{minion.name} мертв, выберите другого персонажа")
+        continue
+    if minion.name == "Кевин":
+        print(f"У {minion.name} сейчас {minion.hp} хп")
+        print("Выберите атаку из двух: 1.1 Удар кулаком 1.2 Удар пятой точкой. Напишите номер атаки")
+    elif minion.name == "Боб":
+        print(f"У {minion.name} сейчас {minion.hp} хп")
+        print("Выберите атаку из двух: 2.1 Крик ПАПАЙЯ 2.2 Вылизывание. Напишите номер атаки")
+    elif minion.name == "Стюарт":
+        print(f"У {minion.name} сейчас {minion.hp} хп")
+        print("Выберите атаку из двух: 3.1 Сделать бобо 3.2 Сделать ауч. Напишите номер атаки")
+
+
+    attack_choice = input()
+
+    if attack_choice == "1.1":
+        minion.hand(hitler, minion.damage, minion.strengh, minion.stiffness)
+    elif attack_choice == "1.2":
+        minion.butt(hitler, minion.damage, minion.dexterity, minion.stiffness)
+    elif attack_choice == "2.1":
+        minion.papaya(hitler, minion.damage, minion.idiocy)
+    elif attack_choice == "2.2":
+        minion.licking(hitler, minion.damage, minion.idiocy, minion.dexterity)
+    elif attack_choice == "3.1":
+        minion.bobo(hitler, stuart.damage, stuart.playfulness, stuart.strengh)
+    elif attack_choice == "3.2":
+        minion.augh(hitler, minion.damage, minion.playfulness)
+    else:
+        print("Неизвестная атака. Ход пропущен.")
+        continue
+
+    hitler.attacking(minion, hitler.damage)
+
+    if minion.hp <= 0:
+        print(f"{minion.name} погиб")
+    else:
+        print(f"У {minion.name} теперь {minion.hp} хп")
 
 print("ура гитлер сдох")
